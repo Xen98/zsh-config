cd017018589eee984301727a502ef32c43301a41
Xen <luisgpm98@gmail.com>
2024-07-11 23:12:50 -0600

    configuraciones de oh-my-zsh pasadas a carpeta de config

---
+++ zshrc.zsh
@@ -0,0 +1,22 @@
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+source $HOME/.config/zsh/aliases.zsh
+
+source $HOME/.config/zsh/env.zsh
+
+source $HOME/.config/zsh/nvm.zsh
+
+source $HOME/.config/zsh/sdkman.zsh
+
+source $HOME/.config/zsh/plugins.zsh
+
+source $ZSH/oh-my-zsh.sh
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
