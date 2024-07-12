d4117b22854dc26434889fed06b2370c81ace082
Xen <luisgpm98@gmail.com>
2024-07-12 01:52:03 -0600

    alias de configuracion nvim y wezterm

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,7 @@
 alias l='ls -l'
 alias ll='ls -la'
 alias nv='nvim'
-alias zshconfig="nvim ~/.zshrc"
 alias ohmyzsh="nvim ~/.oh-my-zsh"
+alias zshconfig="nvim ~/.zshrc"
+alias nvimconfig="nvim ~/.config/nvim/init.lua"
+alias weztermconfig="nvim ~/.wezterm.lua"
